12e0e84e6378893452340df0f2693a6c75bb0f03
xiaoyu2006 <caoyi06@qq.com>
2021-12-18 22:40:23 +0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,83 @@
+extern crate image;
+extern crate num;
+
+use image::png::PNGEncoder;
+use image::ColorType;
+use num::Complex;
+use std::fs::File;
+
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i);
+        }
+    }
+    None
+}
+
+fn pix_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (w, h) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * w / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * h / bounds.1 as f64,
+    }
+}
+
+#[test]
+fn test_pix_to_point() {
+    assert_eq!(
+        pix_to_point(
+            (100, 100),
+            (25, 75),
+            Complex { re: -1.0, im: 1.0 },
+            Complex { re: 1.0, im: -1.0 }
+        ),
+        Complex { re: -0.5, im: -0.5 }
+    )
+}
+
+pub fn render(
+    pixels: &mut [u8],
+    bounds: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) {
+    assert!(pixels.len() == bounds.0 * bounds.1);
+    for row in 0..bounds.1 {
+        for col in 0..bounds.0 {
+            let point = pix_to_point(bounds, (col, row), upper_left, lower_right);
+            pixels[row * bounds.0 + col] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8,
+            }
+        }
+    }
+}
+
+pub fn write_image(
+    filename: &str,
+    pixels: &[u8],
+    bounds: (usize, usize),
+) -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(
+        &pixels,
+        bounds.0 as u32,
+        bounds.1 as u32,
+        ColorType::Gray(8),
+    )?;
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use mandelbrot::*;
+use num::Complex;
+use std::io::Write;
+use std::str::FromStr;
+
+fn parse_pair<T: FromStr>(s: &str, sep: char) -> Option<(T, T)> {
+    match s.find(sep) {
+        None => None,
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
+    }
+}
+
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None,
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("1x2", 'x'), Some((1, 2)));
+    assert_eq!(parse_pair::<i32>("2", 'x'), None);
+    assert_eq!(parse_pair::<i32>(",", ','), None);
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(
+        parse_complex("123,321"),
+        Some(Complex {
+            re: 123.0,
+            im: 321.0
+        })
+    );
+    assert_eq!(parse_complex("123,"), None);
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 5 {
+        writeln!(
+            std::io::stderr(),
+            "Usage: mandelbrot FILE PIXELS UPPER_LEFT LOWER_RIGHT"
+        )
+        .unwrap();
+        writeln!(
+            std::io::stderr(),
+            "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+            args[0]
+        )
+        .unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair::<usize>(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    render(&mut pixels, bounds, upper_left, lower_right);
+
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
+}
